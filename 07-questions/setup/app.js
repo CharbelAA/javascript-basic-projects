a8a7f3d8e1c549ddc8df90d40c0b3c9531c8ee6c
Charbel <CharbelAA@users.noreply.github.com>
2023-02-03 14:26:30 -0500

    TIL = CSS is an important part of the equation.

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,36 @@
 //using selectors inside the element
 // traversing the dom
+const expandQuestion = document.querySelectorAll(".question-btn");
+
+expandQuestion.forEach((element) => {
+  element.addEventListener("click", () => {
+    let questionParent = element.parentElement;
+
+    while (!questionParent.classList.contains("question")) {
+      questionParent = questionParent.parentElement;
+    }
+
+    questionParent.classList.toggle("show-text");
+  });
+});
+
+// The key here is that css classes are passed down to children from the parent.
+// So use combo classes in your css to make all children change when the parent is given a class.
+/* 
+.question-text {
+    display: none;
+  }
+  .show-text .question-text {
+    display: block;
+  }
+  .minus-icon {
+    display: none;
+  }
+  .show-text .minus-icon {
+    display: inline;
+  }
+  .show-text .plus-icon {
+    display: none;
+  }
+
+*/
