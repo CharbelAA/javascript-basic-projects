c1bdac68e267ad7920b51836c7d32919af3a2aa6
Charbel <CharbelAA@users.noreply.github.com>
2023-02-12 18:05:47 -0500

    countdown

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -21,3 +21,24 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadLine = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4"); //get me all the H4s within the deadline-format parent.
+
+let futureDate = new Date(2023, 08, 16, 8, 30, 00);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+
+month = months[month];
+
+let weekday = futureDate.getDay();
+let day = futureDate.getDate();
+
+weekday = weekdays[weekday];
+
+giveaway.textContent = `giveaway ends on  ${weekday}  ${month} ${day}, ${year} @ ${hours}:${minutes}am`;
